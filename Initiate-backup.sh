f9698ba36cc59c80d8e5d6082af6768622b60432
Pasindu Dissanayake <pasindudissanayake@orangehrm.com>
2024-12-17 09:31:18 +0530

    Initial commit with the blue print

---
+++ Initiate-backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a new directory for the backup
+
+
+# Check available space 
+
+## If required space is available ##
+
+## If required space is not available ##
---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove old backup directory
+
+# Create a new backup directory
+
+# Find databases starting with prod_
+
+# Backup process
