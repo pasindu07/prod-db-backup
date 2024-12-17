7cec94068da858c9bb8f99a277e99e54c245faee
Pasindu Dissanayake <pasindudissanayake@orangehrm.com>
2024-12-17 10:46:19 +0530

    Added process aborting function

--- abort-backup.sh
+++ abort-backup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# Inform the failure 
+# Function to send email and log abort message
+abort_backup() {
+    local message=$1
+    echo "Backup aborted: $message" | tee -a "$LOG_FILE"
+    echo -e "Subject: Backup Process Aborted\n\n$message" | sendmail "$EMAIL_RECIPIENT"
+    exit 1
+}
