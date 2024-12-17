c1fc4440bc921ab86b4e694c9463c782b4219e60
Pasindu Dissanayake <pasindudissanayake@orangehrm.com>
2024-12-17 13:25:09 +0530

    Updated server name variable

--- abort_backup.sh
+++ abort_backup.sh
@@ -4,6 +4,6 @@
 abort_backup() {
     local message=$1
     echo "Backup aborted: $message" | tee -a "$LOG_FILE"
-    echo -e "From: backup@orangehrm.com\nSubject: Backup Process Aborted - $SERVER_NAME\n\n$message" | sendmail "$EMAIL_RECIPIENT"
+    echo -e "From: backup@orangehrm.com\nSubject: Backup Process Aborted - $SERVER\n\n$message" | sendmail "$EMAIL_RECIPIENT"
     exit 1
 }
